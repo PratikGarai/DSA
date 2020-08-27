c8af914bf49d85c01d9d051ea774e9128a9332c4
PratikGarai <pratikgarai0208@gmail.com>
2020-08-27 13:14:11 +0530

    Completed the linked list program

--- C/LinkedList.c
+++ C/LinkedList.c
@@ -31,20 +31,43 @@ void push(struct L_list* l, int a)
      }
 }
 
+void push_after(struct L_list* l, int a, int pos)
+{
+     struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+     struct Node* p;
+     n->data = a;
+     p = l->head;
+     int i;
+     for(i=0;i<pos-1;i++)
+         p = p->next;
+     n->next = p->next;
+     p->next = n;
+}
+
+void delete_after(struct L_list* l, int pos)
+{
+     struct Node* p = l->head;
+     int i;
+     for(i=0;i<pos-1;i++)
+         p = p->next;
+     if (p->next==NULL)
+         return;
+     p->next = p->next->next;
+}
+
 void print(struct L_list* l)
 {
      struct Node* p  = l->head;
      while(p!=NULL)
      {
-         printf("%d\t",p->data);
+         printf("%d ",p->data);
          p = p->next;
      }
 }
 
 int main()
 {
-    struct L_list List;
-    
+    struct L_list List; 
     List.head = NULL;
     
     int n,i;
@@ -55,8 +78,6 @@ int main()
         scanf("%d",&a);
         push(&List, a);
     }
-    
     print(&List);
-    
     return 0;
 }
