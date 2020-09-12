82c42af50729789cb8221b68b146cbd0016c7253
PratikGarai <pratikgarai0208@gmail.com>
2020-09-12 06:02:41 +0530

    Added the basic part of linked list

---
+++ DSA (Pratik)/Java/LinkedList/SingleLinkedList.java
@@ -0,0 +1,34 @@
+// Implementation of an integer Lnked List
+
+import java.util.Scanner;
+
+// Class of a single node
+class Node
+{
+	int data;
+	Node next;
+
+	Node(int el)
+	{
+		data = el;
+		next = null;
+	}
+}
+
+// Class of the Linked list
+class List
+{
+	Node head, tail;
+	List()
+	{
+		head = tail = null;
+	}
+}
+
+public class SingleLinkedList
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+	}
+}
