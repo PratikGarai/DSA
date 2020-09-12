6396c5c50307716afa3e9c0fcde674a8a363a27c
PratikGarai <pratikgarai0208@gmail.com>
2020-09-12 08:35:59 +0530

    Added the insert and print class

--- DSA (Pratik)/Java/LinkedList/DoublyLinkedList.java
+++ DSA (Pratik)/Java/LinkedList/DoublyLinkedList.java
@@ -14,12 +14,52 @@ class Node
 
 class List
 {
-
 	Node head, tail;
 
 	List()
 	{
 		head = next = null;
+	}
+
+	void addElement(int data)
+	{
+		Node a = new Node(data);
+		if(isEmpty())
+		{
+			head = tail = a;
+			return;
+		}
+		tail.next = a;
+		a.prev = tail;
+		tail = a;
+	}
+
+	boolean isEmpty()
+	{
+		return head==null;
+	}
+
+	void printList(boolean reverse)
+	{
+		if(!reverse)
+		{
+			Node current = head;
+			while(current!=null)
+			{
+				System.out.print(current.data+"  ");
+				current = current.next;
+			}
+		}
+		else
+		{
+			Node current = tail;
+			while(current!=null)
+			{
+				System.out.print(current.data+"  ");
+				current = current.prev;
+			}
+		}
+		System.out.println();
 	}
 }
 
@@ -28,5 +68,9 @@ public class DoublyLinkedList
 	public static void main(String[] args)
 	{
 		List l = new List();
+		l.addElement(6);
+		l.addElement(4);
+		l.addElement(8);
+		l.printList();
 	}
 }
