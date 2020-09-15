aa01a07d530b6d8ea39273609abb8452e0a203ac
PratikGarai <pratikgarai0208@gmail.com>
2020-09-15 22:29:59 +0530

    Started code of binary search tree

---
+++ DSA (Pratik)/Java/Tree/BinarySearchTree/BasicBST.java
@@ -0,0 +1,29 @@
+// Implementation of the basic integer binary Serch tree in java
+
+class Node
+{
+	Node left, right;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		left=right=null;
+	}
+}
+
+class Tree
+{
+	Node root;
+	Tree()
+	{
+		root = null;
+	}
+}
+
+public class BasicBST
+{
+	public static void main(String[] args)
+	{
+		Tree a = new Tree();
+	}
+}
