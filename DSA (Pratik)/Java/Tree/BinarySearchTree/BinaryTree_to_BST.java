37129c59451c6a9a713658a0697e044cba95ff89
PratikGarai <pratikgarai0208@gmail.com>
2020-09-16 17:06:37 +0530

    Added the basic tree

---
+++ DSA (Pratik)/Java/Tree/BinarySearchTree/BinaryTree_to_BST.java
@@ -0,0 +1,66 @@
+class Node
+{
+	Node left, right;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		left=right=null;
+	}
+}
+
+class Tree
+{
+	Node root;
+	Tree()
+	{
+		root = null;
+	}
+
+	void insert(int d)
+	{
+		if(isEmpty())
+			root = new Node(d);
+		else
+		{
+			Node current = root;
+			Node a = new Node(d);
+			while(true)
+			{
+				if(d<current.data && current.left==null)
+				{
+					current.left = a;
+					break;
+				}
+				else if(d>=current.data && current.right==null)
+				{
+					current.right = a;
+					break;
+				}
+				else if(d<current.data)
+					current = current.left;
+				else
+					current = current.right;
+			}
+		}
+	}
+
+	boolean search(int data)
+	{
+		Node current = root;
+		while(current!=null)
+		{
+			if(data<current.data)
+				current = current.left;
+			else if(data>current.data)
+				current = current.right;
+			else return true;
+		}
+		return false;
+	}
+
+	boolean isEmpty()
+	{
+		return(root==null);
+	}
+}
