6962b80e4248f0742cebe339c74f9a0bd44c5794
PratikGarai <pratikgarai0208@gmail.com>
2020-09-17 06:01:38 +0530

    Added the new insertion code

--- DSA (Pratik)/Java/Tree/BinarySearchTree/BinaryTree_to_BST.java
+++ DSA (Pratik)/Java/Tree/BinarySearchTree/BinaryTree_to_BST.java
@@ -6,9 +6,10 @@
  * 		from the root node and moves to the appropriate child based on
  * 		the given directions. However, if the control encounters a null
  * 		node, it inserts the element there. However, if the node reached
- * 		by the end of the sequence is not empty, the function returns
- * 		false and prints an error message. If the insertion is successful
- * 		it returns true.
+ * 		by the end of the sequence is not empty or the string contains
+ * 		alphabets other than 'L' and 'R' (case sensitive), the function 
+ * 		returns false and prints an error message. If the insertion is 
+ * 		successful it returns true.
  * 	2. int data : The data to be inserted.
 */
 
@@ -38,6 +39,31 @@ class Tree
 
 	boolean insert(String directions, int data)
 	{
+		Node current = root;
+		int i=0, n=directions.length();
+		for(i=0;i<n;i++)
+		{
+			if(directions.charAt(i)=='L')
+			{
+				if(current.left==null)
+				{
+					current.left = new Node(data);
+					return True;
+				}
+				current = current.left;
+			}
+			else if(directions.charAt(i)=='R')
+			{
+				if(current.right==null)
+				{
+					current.right = new Node(data);
+					return True;
+				}
+				current = current.right;
+			}
+			else return false;
+		}
+		return false;
 	}
 	
 	void printInorder(Node current)
@@ -61,6 +87,5 @@ public class BinaryTree_to _BST
 {
 	public static void main(String[] args)
 	{
-
 	}
 }
