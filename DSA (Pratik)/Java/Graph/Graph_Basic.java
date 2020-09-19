8345e3707a174337e91b5ebce9084d5c12158398
PratikGarai <pratikgarai0208@gmail.com>
2020-09-19 11:13:56 +0530

    Simple methods added

--- DSA (Pratik)/Java/Graph/Graph_Basic.java
+++ DSA (Pratik)/Java/Graph/Graph_Basic.java
@@ -1,14 +1,34 @@
-// Basic implementation of a graph in java
+// Basic implementation of an integer undirected graph in java
+// Only numbers >=0 are allowed
 
 import java.util.*;
 
 class Node
 {
-	int data;
 	Vector<Node> neighbours;
 	Node(int d)
 	{
-		data = d;
 		neighbours = new Vector<Node>;
+	}
+}
+
+class Graph
+{
+	Vector<Node> nodes;
+	Graph()
+	{
+		nodes = new Vector<Node>;
+	}
+
+	void addEdge(int d1, int d2)
+	{
+		Node a = new Node();
+		Node b = new Node();
+		if(d1<nodes.size())
+			if(nodes.get(d1)==null)
+				nodes.insertElementAt(a, d1);
+		if(d2<nodes.size())
+			if(nodes.get(d2)==null)
+				nodes.insertElementAt(b, d2);
 	}
 }
