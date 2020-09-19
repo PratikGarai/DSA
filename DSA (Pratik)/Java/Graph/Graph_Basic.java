5c73fa17adaa4310db1ee762c674ff3788d27abe
PratikGarai <pratikgarai0208@gmail.com>
2020-09-19 09:57:15 +0530

    Added the node class

---
+++ DSA (Pratik)/Java/Graph/Graph_Basic.java
@@ -0,0 +1,14 @@
+// Basic implementation of a graph in java
+
+import java.util.*;
+
+class Node
+{
+	int data;
+	Vector<Node> neighbours;
+	Node(int d)
+	{
+		data = d;
+		neighbours = new Vector<Node>;
+	}
+}
