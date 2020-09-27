033cbd275fd12523e6df27293dc204791bef524e
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 13:29:44 +0530

    Syntax error corrected

--- DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
+++ DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
@@ -8,27 +8,27 @@ class permute
 	int l, print;
 	permute(String a, int b)
 	{
-		str = a;
 		l = a.length();
 		print = b;
-		sort_string(str);
+		str = sort_string(a);
 	}
 
-	void sort_string()
+	String sort_string(String a)
 	{
 		char st[] = new char[l];
-		for(int i=0;i<n;i++)
+		for(int i=0;i<l;i++)
 			st[i] = a.charAt(i);
 
 		// using bubble sort
-		for(int i=0;i<n-1;i++)
-			for(int j=i;j<n;j++)
+		for(int i=0;i<l-1;i++)
+			for(int j=i;j<l;j++)
 				if(st[i]>st[j])
 				{
 					char temp = st[i];
 					st[i] = st[j];
 					st[j] = temp;
 				}
+		return String.valueOf(st);
 	}
 
 	int getPermutations(int ind)
@@ -38,9 +38,9 @@ class permute
 			return 1;
 		for(int i=ind+1;i<l;i++)
 		{
-			str = str.substring(0,ind)+str.substring(ind,ind+1)+str.substring(ind,i)+str.substring(i,i+1)+substring(i+1,l);
+			str = str.substring(0,ind)+str.substring(ind,ind+1)+str.substring(ind,i)+str.substring(i,i+1)+str.substring(i+1,l);
 			count += getPermutations(ind+1);
-			str = str.substring(0,ind)+str.substring(i,i+1)+str.substring(ind,i)+str.substring(ind,ind+1)+substring(i+1,l);
+			str = str.substring(0,ind)+str.substring(i,i+1)+str.substring(ind,i)+str.substring(ind,ind+1)+str.substring(i+1,l);
 		}
 		return count;
 	}
@@ -51,7 +51,7 @@ public class StringPermutationNonRepeat
 	public static void main(String[] args)
 	{
 		String a = args[0];
-		String b = args[1];
+		int b = Integer.valueOf(args[1]);
 		System.out.println("The entered string : "+a);
 		permute ob = new permute(a,b);
 		int p = ob.getPermutations(0);
