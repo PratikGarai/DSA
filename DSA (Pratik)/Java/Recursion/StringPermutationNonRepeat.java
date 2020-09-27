942d65ec36cebd3bc2c3404b5beb052e85914825
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 13:01:26 +0530

    sorter code added

--- DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
+++ DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
@@ -1,4 +1,5 @@
-// Non repeting permutations problem. Takes in input from command line.
+// Non repetingi character permutations problem. 
+// Takes in input from command line.
 // The string is case sensitive.
 
 class permute
@@ -12,6 +13,20 @@ class permute
 		l = a.length();
 		for(int i=0;i<n;i++)
 			st[i] = a.charAt(i);
+		sort_string();
+	}
+
+	void sort_string()
+	{
+		// using bubble sort
+		for(int i=0;i<n-1;i++)
+			for(int j=i;j<n;j++)
+				if(st[i]>st[j])
+				{
+					char temp = st[i];
+					st[i] = st[j];
+					st[j] = temp;
+				}
 	}
 }
 
