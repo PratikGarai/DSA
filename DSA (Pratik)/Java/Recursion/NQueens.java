e72b860270fc971c7b9bd8709718bc972c7d3ac6
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 11:49:48 +0530

    More code

--- DSA (Pratik)/Java/Recursion/NQueens.java
+++ DSA (Pratik)/Java/Recursion/NQueens.java
@@ -1,9 +1,27 @@
 class Queens
 {
 	int n;
+	int matrix[][];
+	int rows[], cols[], diagl[], diagr[];
 	Queens(int s)
 	{
 		n = s;
+		matrix = new int[n][n];
+		rows = int[n];
+		cols = int[n];
+		diagl = int[n];
+		diagr = int[n];
+	}
+
+	int putQueen(int r, int c)
+	{
+		count = 0;
+		if(r==n)
+			return 1;
+		else
+		{
+			for(int 
+		}
 	}
 }
 
@@ -13,6 +31,8 @@ public class NQueens
 	{
 		int n = Integer.valueOf(args[0]);
 		System.out.println("N selected : "+n);
-		Queens = new Queens(n);
+		Queens ob = new Queens(n);
+		int result = 0;
+		System.out.println("The result is : "+result);
 	}
 }
