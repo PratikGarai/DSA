bc0d2316eb357f8ae44b1dfe59e67bb9f7fd50ff
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 13:24:29 +0530

    Added the trial permutations code

--- DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
+++ DSA (Pratik)/Java/Recursion/StringPermutationNonRepeat.java
@@ -5,19 +5,21 @@
 class permute
 {
 	String str;
-	char st[];
-	int l;
-	permute(String a)
+	int l, print;
+	permute(String a, int b)
 	{
 		str = a;
 		l = a.length();
-		for(int i=0;i<n;i++)
-			st[i] = a.charAt(i);
-		sort_string();
+		print = b;
+		sort_string(str);
 	}
 
 	void sort_string()
 	{
+		char st[] = new char[l];
+		for(int i=0;i<n;i++)
+			st[i] = a.charAt(i);
+
 		// using bubble sort
 		for(int i=0;i<n-1;i++)
 			for(int j=i;j<n;j++)
@@ -28,6 +30,20 @@ class permute
 					st[j] = temp;
 				}
 	}
+
+	int getPermutations(int ind)
+	{
+		int count = 0;
+		if(ind == l-1)
+			return 1;
+		for(int i=ind+1;i<l;i++)
+		{
+			str = str.substring(0,ind)+str.substring(ind,ind+1)+str.substring(ind,i)+str.substring(i,i+1)+substring(i+1,l);
+			count += getPermutations(ind+1);
+			str = str.substring(0,ind)+str.substring(i,i+1)+str.substring(ind,i)+str.substring(ind,ind+1)+substring(i+1,l);
+		}
+		return count;
+	}
 }
 
 public class StringPermutationNonRepeat
@@ -35,7 +51,9 @@ public class StringPermutationNonRepeat
 	public static void main(String[] args)
 	{
 		String a = args[0];
+		String b = args[1];
 		System.out.println("The entered string : "+a);
-		int p = getPermutations(a);
+		permute ob = new permute(a,b);
+		int p = ob.getPermutations(0);
 	}
 }
