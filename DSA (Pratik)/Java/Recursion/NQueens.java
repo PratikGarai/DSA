ce77a76b5b239332b8e4bb8a172619338fcb8ba8
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 12:38:34 +0530

    Final commit for nqueens

--- DSA (Pratik)/Java/Recursion/NQueens.java
+++ DSA (Pratik)/Java/Recursion/NQueens.java
@@ -1,9 +1,9 @@
 class Queens
 {
-	int n;
+	int n, print;
 	int matrix[][];
 	int rows[], cols[], diagl[], diagr[];
-	Queens(int s)
+	Queens(int s, int p)
 	{
 		n = s;
 		matrix = new int[n][n];
@@ -11,6 +11,7 @@ class Queens
 		cols = new int[n];
 		diagl = new int[2*n-1];
 		diagr = new int[2*n-1];
+		print = p;
 	}
 
 	int putQueen(int r, int c)
@@ -19,18 +20,26 @@ class Queens
 		if(rows[r]==1 || cols[c]==1 || diagl[r+c]==1 || diagr[r-c+n-1]==1)
 			return 0;
 		if(r==n-1)
+		{
+			matrix[r][c] = 1;
+			if(print==1)
+				printMatrix();
+			matrix[r][c] = 0;
 			return 1;
+		}
 
 		rows[r] = 1;
 		cols[c] = 1;
 		diagl[r+c] = 1;
 		diagr[r-c+n-1] = 1;
+		matrix[r][c] = 1;
 		for(int i=0;i<n;i++)
 			count += putQueen(r+1, i);
 		rows[r] = 0;
 		cols[c] = 0;
 		diagl[r+c] = 0;
 		diagr[r-c+n-1] = 0;
+		matrix[r][c] = 0;
 		return count;
 	}
 
@@ -42,6 +51,16 @@ class Queens
 		return result;
 	}
 
+	void printMatrix()
+	{
+		System.out.println("\n");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+				System.out.print(matrix[i][j]+" ");
+			System.out.println();
+		}
+	}
 }
 
 public class NQueens
@@ -49,8 +68,9 @@ public class NQueens
 	public static void main(String[] args)
 	{
 		int n = Integer.valueOf(args[0]);
+		int m = Integer.valueOf(args[1]);
 		System.out.println("N selected : "+n);
-		Queens ob = new Queens(n);
+		Queens ob = new Queens(n, m);
 		int result = ob.getResult();
 		System.out.println("The result is : "+result);
 	}
