3b527ca0df8ad6edc7c148d5a0fa4c4b2aa6a019
PratikGarai <pratikgarai0208@gmail.com>
2020-09-27 12:19:31 +0530

    Completed nquuens

--- DSA (Pratik)/Java/Recursion/NQueens.java
+++ DSA (Pratik)/Java/Recursion/NQueens.java
@@ -7,28 +7,41 @@ class Queens
 	{
 		n = s;
 		matrix = new int[n][n];
-		rows = int[n];
-		cols = int[n];
-		diagl = int[2*n-1];
-		diagr = int[2*n-1];
+		rows = new int[n];
+		cols = new int[n];
+		diagl = new int[2*n-1];
+		diagr = new int[2*n-1];
 	}
 
 	int putQueen(int r, int c)
 	{
-		count = 0;
-		if(rows[r]==1 || cols[c]==1 || diagl[r+c]==1 || diagr[r-c+n]==1)
+		int count = 0;
+		if(rows[r]==1 || cols[c]==1 || diagl[r+c]==1 || diagr[r-c+n-1]==1)
 			return 0;
-		if(r==n)
+		if(r==n-1)
 			return 1;
 
 		rows[r] = 1;
 		cols[c] = 1;
 		diagl[r+c] = 1;
-		diagr[r-c+n] = 1;
+		diagr[r-c+n-1] = 1;
 		for(int i=0;i<n;i++)
 			count += putQueen(r+1, i);
+		rows[r] = 0;
+		cols[c] = 0;
+		diagl[r+c] = 0;
+		diagr[r-c+n-1] = 0;
 		return count;
 	}
+
+	int getResult()
+	{
+		int result = 0;
+		for(int i=0; i<n; i++)
+			result += putQueen(0,i);
+		return result;
+	}
+
 }
 
 public class NQueens
@@ -38,7 +51,7 @@ public class NQueens
 		int n = Integer.valueOf(args[0]);
 		System.out.println("N selected : "+n);
 		Queens ob = new Queens(n);
-		int result = 0;
+		int result = ob.getResult();
 		System.out.println("The result is : "+result);
 	}
 }
