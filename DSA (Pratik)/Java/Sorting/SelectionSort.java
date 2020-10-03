afdd63f9051e70ff4489bb49b4eeb31e2719c3d4
PratikGarai <pratikgarai0208@gmail.com>
2020-10-03 12:36:27 +0530

    Added selection sort code

---
+++ DSA (Pratik)/Java/Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SelectionSort
+{
+	public static void main(String[] args)
+	{
+		Scanner in =  new Scanner(System.in);
+		System.out.println("Enter the number of elements");
+		int n = in.nextInt();
+		int a[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+			System.out.print("Enter the "+(i+1)+" th number :  ");
+			a[i] = in.nextInt();
+		}
+		int m_index, temp;
+		for(int i=0;i<n-1;i++)
+		{
+			m_index = 0;
+			for(int j=1;j<n-i;j++)
+			{
+				if(a[j]>a[m_index])
+					m_index = j;
+			}
+			temp = a[m_index];
+			a[m_index] = a[n-i-1];
+			a[n-i-1] = temp;
+		}
+		System.out.print("Sorted array : ");
+		for(int i=0;i<n;i++)
+			System.out.print(a[i]+"  ");
+		System.out.println();
+	}
+}
