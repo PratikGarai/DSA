c920782d0fbc967f6c871331ecf97550983ebb00
PratikGarai <pratikgarai0208@gmail.com>
2020-10-09 09:25:06 +0530

    Added the basic template

---
+++ DSA (Pratik)/C_Cpp/LinkedList/LinkedListBasic.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(null);
+
+	return 0;
+}
---
+++ DSA (Pratik)/C_Cpp/Template.cpp
@@ -0,0 +1,13 @@
+//Template
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(null);
+
+	return 0;
+}
