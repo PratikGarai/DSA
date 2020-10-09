80fec1d16cd752946209970350c36407274dd8ae
PratikGarai <pratikgarai0208@gmail.com>
2020-10-09 10:30:32 +0530

    Basic stuff added

--- DSA (Pratik)/C_Cpp/LinkedList/LinkedListBasic.cpp
+++ DSA (Pratik)/C_Cpp/LinkedList/LinkedListBasic.cpp
@@ -6,32 +6,60 @@ class Node
 	public:
 		int data;
 		Node* next;
+		
+		Node(int x){
+			data = x;
+			next = NULL;
+		}
 };
 
-class List
+class LinkedList
 {
 	private:
 		Node* head;
-		Node* last;
+		Node* tail;
 	
 	public:
-		Node()
-		{
+		LinkedList(){
 			head = NULL;
-			last = head;
-			cout<<"List created\n";
+			tail = head;
 		}
 		
-}
+		void add(int x){
+			if(head==NULL && tail==NULL){
+				head = new Node(x);
+				tail = head;
+			}
+			else{
+				Node* current = new Node(x);
+				tail->next = current;
+				tail = current;
+			}
+		}
+
+		void print(){
+			Node* current = head;
+			while(current!=NULL){
+				cout<<current->data<<"  ";
+				current = current->next;
+			}
+			cout<<endl;
+		}
+};
 
 int main()
 {
-
+	LinkedList* l = new LinkedList();
 	int n, x;
-	for(int i=0;i<n;i++)
-	{
+	cout<<"Enter the number of elements : ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Enter the "<<i+1<<"th element : "; 
 		cin>>x;
+		l->add(x);
 	}
+	cout<<"Contents of list : ";
+	l->print();
 
-	return 0;
+	return n;
 }
