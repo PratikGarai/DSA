6a1b5a4eb3af1145c492bc7b660678d544b0c3e2
PratikGarai <pratikgarai0208@gmail.com>
2020-10-17 13:19:40 +0530

    stack started

---
+++ DSA (Pratik)/C_Cpp/Stack/Stack.h
@@ -0,0 +1,8 @@
+class Stack
+{
+	public :
+		int* a;
+		Stack(int l){
+			a = new int[l];
+		}
+};
---
+++ DSA (Pratik)/C_Cpp/Stack/Stack_Basic.cpp
@@ -0,0 +1,18 @@
+#include"Stack.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the number of elements to enter : ";
+	cin>>n;
+	Stack* ob = new Stack(n);
+	bool loop = true;
+	while(loop)
+	{
+		loop = false;
+	}
+	cout<<"Bye!\n";
+
+	return 0;
+}
