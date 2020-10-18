c2b4f12c98886d3ec694d1993701c64a813a8a41
PratikGarai <pratikgarai0208@gmail.com>
2020-10-18 13:06:19 +0530

    Added some basi validation to infix postfix

--- DSA (Pratik)/C_Cpp/Stack/Infix_to_Postix.cpp
+++ DSA (Pratik)/C_Cpp/Stack/Infix_to_Postix.cpp
@@ -41,6 +41,29 @@ void generatePostfix(char *arr, int l)
 	}
 }
 
+bool validateExpression(char *arr, int l)
+{
+	int i;
+	int parantheses = 0;
+	for(i=0;i<l;i++)
+	{
+		if(isalpha(arr[i])||arr[i]=='+'||arr[i]=='-'||arr[i]=='/'||arr[i]=='*')
+			continue;
+		else if(arr[i]=='(')
+			parantheses++;
+		else if(arr[i]==')'){
+			parantheses--;
+			if(parantheses<0)
+				return false;
+		}
+		else
+			return false;
+	}
+	if(parantheses>0)
+		return false;
+	return true;
+}
+
 int main()
 {
 	int l;
@@ -50,8 +73,13 @@ int main()
 	cout<<"Enter the expression : ";
 	cin>>str;
 	str[l] = '\0';
-	cout<<"The postfix form :";
-	generatePostfix(str, l);
+	if(validateExpression(str, l))
+	{
+		cout<<"The postfix form :";
+		generatePostfix(str, l);
+	}
+	else
+		cout<<"Invalid expression";
 
 	return 0;
 }
