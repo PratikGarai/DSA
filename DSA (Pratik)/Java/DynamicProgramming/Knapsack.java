b1aa714ffd93339c3c93f2f56cd482dd225ea425
PratikGarai <pratikgarai0208@gmail.com>
2020-11-24 17:56:06 +0530

    Added recursive code

--- DSA (Pratik)/Java/DynamicProgramming/Knapsack.java
+++ DSA (Pratik)/Java/DynamicProgramming/Knapsack.java
@@ -9,6 +9,17 @@ class Knapsack
 		weights = w;
 	}
 
+	int recursive(int n, int cap)
+	{
+		if(cap==0||n==-1)
+			return 0;
+
+		if(weights[n]<=cap)
+			return Math.max( recursive(n-1,cap), values[n]+recursive(n-1,cap-weights[n]));
+		else
+			return recursive(n-1, cap);
+	}
+
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
@@ -25,6 +36,12 @@ class Knapsack
 			w[i] = in.nextInt();
 		}
 
+		System.out.print("Enter the capacity of the knapsack : ");
+		int cap = in.nextInt();
+
 		Knapsack ob = new Knapsack(v,w);
+		
+		int res = ob.recursive(n-1, cap);
+		System.out.println("The maximum value is : "+res);
 	}
 }
