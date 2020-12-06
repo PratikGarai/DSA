7ca81a705e8b2aa09400e64db2968ba05a5cb0d8
PratikGarai <pratikgarai0208@gmail.com>
2020-12-06 10:56:18 +0530

    Added weighted graph

---
+++ DSA (Pratik)/Java/Graph/Graphs/MatrixGraphWeighted.java
@@ -0,0 +1,47 @@
+package Graphs;
+import java.util.Scanner;
+
+public class MatrixGraphWeighted
+{
+	public int[][] v;
+	public int l;
+	public boolean isDirected;
+
+	public MatrixGraphWeighted(int a, boolean b)
+	{
+		isDirected = b;
+		v =  new int[a][a];
+		l = a;
+	}
+
+	public void addEdge(int a, int b, int w)
+	{
+		v[a][b] = w;
+		if(!isDirected)
+			v[b][a] = w;
+	}
+
+	public void acceptor()
+	{
+		Scanner in = new Scanner(System.in);
+		for(int i=0;i<l;i++)
+		{
+			for(int j=0;j<l;j++)
+			{
+				v[i][j] = in.nextInt();
+			}
+		}
+	}
+
+	public void printMatrix()
+	{
+		for(int i=0;i<l;i++)
+		{
+			for(int j=0;j<l;j++)
+			{
+				System.out.print(v[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
